ed1c9fdf38ea5d5f0708b65fb7020a5864aa94b3
Norbert Hamar <norbert.hamar@prestagroup.com>
2023-11-21 16:22:57 +0100

    atalakitasok

---
+++ felhasznalokx.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE `felhasznalok`
+CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE `felhasznalok`;
+
+CREATE TABLE `felhasznalok` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `csaladi_nev` varchar(45) NOT NULL default '',
+  `uto_nev` varchar(45) NOT NULL default '',
+  `bejelentkezes` varchar(12) NOT NULL default '',
+  `jelszo` varchar(40) NOT NULL default '',
+  PRIMARY KEY  (`id`)
+)
+ENGINE = MYISAM
+CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+INSERT INTO `felhasznalok` (`id`,`csaladi_nev`,`uto_nev`,`bejelentkezes`,`jelszo`) VALUES 
+ (1,'admin','admin','admin',sha1('welcome'));
